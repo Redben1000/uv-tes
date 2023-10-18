5a0b4dfe50489b3d65568ed0cf28b20664a7dfc3
Gamer <110696589+Redben1000@users.noreply.github.com>
2023-10-17 20:02:15 -0400

    a

---
+++ Scripts/uv/sw.js
@@ -0,0 +1,1 @@
+importScripts('uv.bundle.js');importScripts('uv.config.js');importScripts('uv.sw.js');const sw=new UVServiceWorker();globalThis.addEventListener('fetch',(event)=>{event.respondWith(sw.fetch(event));});
